9fa93131d6db952c303f797745ae0c201a908d6f
Ferdinando Romano <fmromano@vt.edu>
2014-12-04 18:50:10 -0500

    Finished function to update core availability matrix

--- updateCoreAvailabilityMatrix.m
+++ updateCoreAvailabilityMatrix.m
@@ -1,4 +1,4 @@
-function coreAvailabilityMatrix = updateCoreAvailabilityMatrix(coreAvailabilityMatrix,speedMatrix,maxNumCoresMatrix,matchingMatrix)
+function [coreAvailabilityMatrix,matchedJob] = updateCoreAvailabilityMatrix(coreAvailabilityMatrix,speedMatrix,maxNumCoresMatrix,matchingMatrix)
 
     coreAvailabilityMatrix = []
     if nargin>=1 & nargin < 4
@@ -28,6 +28,64 @@ function coreAvailabilityMatrix = updateCoreAvailabilityMatrix(coreAvailabilityM
     % The nth element is the max number of cores that can be used by job n.
     % The number of columns should be the number of jobs.
     maxNumCoresMatrix = [8,20,10,1,50]
+
+    % The only nonzero element of the nth row is the job number that has been assigned to the nth computer.
+    matchingMatrix = [
+     0,0,0,4,0; ...
+     0,0,0,0,5; ...
+     0,0,3,0,0; ...
+     1,0,0,0,0; ...
+     0,2,0,0,0; ...
+     ]
+
     end  
+
+    totalNumComps = size(matchingMatrix,1)
+
+    matchedJobs = sum(matchingMatrix,2)
+    % Check to be sure there is at least one matching
+    if sum(matchedJobs,1) ==0
+        error('There is no one matched!')
+    end
+
+    % TODO FIXME: Be sure that when calling CollegeAdmissionsGame, 
+    % if a computer doesn't have any cores left, then to set its quota to 0.
+
+    % Of the jobs that were matched, which uses the most cores?
+    reducedMaxNumCoresMatrix = zeros(1,size(matchedJobs,1))
+    for x = 1:totalNumComps
+        if matchedJobs(x) > 0
+            reducedMaxNumCoresMatrix(x) = maxNumCoresMatrix(matchedJobs(x))
+        end
+    end
+    [maxNumCores, jobMostCores] = max(reducedMaxNumCoresMatrix)
+    matchedJob = matchedJobs(jobMostCores)
+
+    % Which computer is matchedJob assigned to?
+    compOfJobMostCores = find(matchedJobs==matchedJob)
+    % How many cores does that computer actually have available?
+    totalCoresAvailable = sum(coreAvailabilityMatrix(compOfJobMostCores,:))
+
+    % How many cores is matchedJob actually able to use?
+    if totalCoresAvailable<maxNumCores
+        numCoresUsed = totalCoresAvailable
+    else
+        numCoresUsed = maxNumCores
+    end
+
+    [coreSpeedsSorted,coreSpeedsSortedIndices] = sort(speedMatrix(matchedJob,:),'descend')
+    for x = 1:numCoresUsed
+        while coreAvailabilityMatrix(compOfJobMostCores,coreSpeedsSortedIndices(x))>0
+            coreAvailabilityMatrix(compOfJobMostCores,coreSpeedsSortedIndices(x)) = coreAvailabilityMatrix(compOfJobMostCores,coreSpeedsSortedIndices(x)) - 1;
+            numCoresUsed = numCoresUsed - 1;
+            if numCoresUsed == 0
+                break
+            end
+        end
+        if numCoresUsed == 0
+            break
+        end
+    end
+            
 
 end
