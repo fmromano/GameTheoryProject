1fd79f8cf0575a2034c4dc6cbd8685ad570967a8
kphines <kphines42@gmail.com>
2014-12-04 16:50:32 -0500

    Updated quota and leftovers

--- GameTheoryProject_KPH/example1.m
+++ GameTheoryProject_KPH/example1.m
@@ -13,6 +13,8 @@ women_pref = zeros(N,N);    % Preference order for the women
 men_pref = [4 1 2 3; 2 3 1 4; 2 4 3 1; 3 1 4 2];
 women_pref = [4 1 3 2; 1 3 2 4; 1 2 3 4; 4 1 3 2];
 
-resultMatrix = collegeAdmissionsGame(men_pref,women_pref,[1 1 1 1]);
+%resultMatrix = collegeAdmissionsGame(men_pref,women_pref,[1 1 1 1]);
 
-resultMatrix2 = collegeAdmissionsGame(men_pref,women_pref,[1 2 1 1]);
+%resultMatrix2 = collegeAdmissionsGame(men_pref,women_pref,[1 2 1 1]);
+
+resultMatrix3 = collegeAdmissionsGame(men_pref,women_pref,[1 1 1 0]);
