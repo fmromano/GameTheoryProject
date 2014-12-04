16ed5de2679558c0f8a8a4c6d6ca603cd9b90a5f
kphines <kphines42@gmail.com>
2014-12-04 10:45:24 -0500

    Fixed extra stop condition and added comments
    This currently works with the examples.

--- GameTheoryProject_KPH/collegeAdmissionsGame.m
+++ GameTheoryProject_KPH/collegeAdmissionsGame.m
@@ -3,11 +3,16 @@ function [responseMatrix] = collegeAdmissionsGame(applicantPref, ...
                                           
 %Help documentation here
 %Assuming applicants apply first
-%Assumption:
-%A = 1st 2nd 3rd ...
-%B = 1st 2nd 3rd ...
+%Matrix setup assumption:
+%A = [1st 2nd 3rd ...]
+%B = [1st 2nd 3rd ...]
 %And so on for both the applicants and institutions.
 
+%To do:
+%1. Test this more.  I have only tried this with easy examples.
+%2. Provide a leftovers matrix for when there are extra applicants.
+
+%Set vectors and matrices
 numAppl = length(applicantPref(:,1));
 numInst = length(institutionPref(1,:));
 applyMat = zeros(numAppl,numInst);
@@ -16,9 +21,10 @@ responseMatrix = applyMat;
 applFree = ones(numAppl,1);
 instFree = ones(numInst,1);
 
+%Main stop parameter for the while loop
 allMatched = 0;
 
-%Continue matching until all members are paired.
+%Continue matching until either all applicants or institutions are taken.
 while allMatched == 0
     
     %Have the applicants propose to the institutions.
@@ -39,12 +45,13 @@ while allMatched == 0
         elseif quota == nnz(applyMat(iLoop,:))
             tentAcceptMat(iLoop,:) = applyMat(iLoop,:);
             instFree(iLoop) = 0;
-        end
+        end %end if below or at the quota
         
         if quota < nnz(applyMat(iLoop,:))
             tempList = applyMat(iLoop,:);
             instPref = institutionPref(iLoop,:);
             
+            %Find out how each preference ranks compared to the others
             ranked = zeros(length(tempList));
             for jLoop = 1:numInst
                 ranked(jLoop,:) = instPref == tempList(jLoop); 
@@ -59,10 +66,22 @@ while allMatched == 0
                 index = find(keptAppl(kLoop)==tempList);
                 tempZeroedList(index) = tempList(index);
             end
+            
+            %Put the kept applicants back into the applyMat
             applyMat(iLoop,:) = tempZeroedList;
             instFree(iLoop) = 0;
-        end
-    end
+        end %end if above quota
+        
+        %See which applicants are currently taken...
+        for lLoop = 1:numAppl
+            testAppl = sum(sum(applyMat == lLoop));
+            if testAppl > 0
+                applFree(lLoop) = 0;
+            elseif testAppl == 0
+                applFree(lLoop) = 1;
+            end            
+        end %for lLoop = 1:numAppl
+    end %for iLoop = 1:numInst
     
     
     %Assemble the response matrix
@@ -76,4 +95,4 @@ end %while allMatched == 0
 
 disp(responseMatrix);
 
-end %function
+end %end of the function
