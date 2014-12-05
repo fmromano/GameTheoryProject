c7503644c7e3f058ce638f8ee0c0ce7311726cfe
kphines <kphines42@gmail.com>
2014-12-05 15:15:37 -0500

    Beginning of quick sort and simple match
    These functions will be used for comparison purposes.  They are
    currently just comments.

---
+++ src/ExampleFiles/quickSort.m
@@ -0,0 +1,7 @@
+%Quicksort function
+%Matchings jobs and computers based on job preference only.  This is a
+%waiting list setup.
+
+
+%Output the matches, leftovers, and efficiency (will be used to see how
+%well this method works).
