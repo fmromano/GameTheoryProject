0066379de1eb0fc96fd2bc727728de077392b44b
kphines <kphines42@gmail.com>
2014-12-06 14:51:57 -0500

    Updated Quicksort

--- src/ExampleFiles/quickSort.m
+++ src/ExampleFiles/quickSort.m
@@ -1,6 +1,9 @@
 %Quicksort function
+
+
 %Matchings jobs and computers based on job preference only.  This is a
 %waiting list setup.
+jobPrefs = jobPreferences(coreAvailabilityMatrix, speedMatrix, maxNumCoresMatrix)
 
 
 %Output the matches, leftovers, and efficiency (will be used to see how
